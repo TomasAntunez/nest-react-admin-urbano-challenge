9c5c7167a666b16168f0dd2fcaf2fdc14b0f7b28
Chuli <tomasantunez.dev@gmail.com>
2025-08-09 18:29:50 -0300

    chore: refactor Dockerfile on backend app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
   await createAdminOnFirstUse();
 
   const configService = app.get(ConfigService);
-  const port = Number(configService.get<string>('BACKEND_PORT')) || 5000;
+  const port = Number(configService.get<string>('BACKEND_PORT')) || 3000;
 
   await app.listen(port);
 }
